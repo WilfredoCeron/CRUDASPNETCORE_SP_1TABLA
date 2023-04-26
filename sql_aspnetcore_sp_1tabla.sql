483d216528afbb32c2f4137b71fd8a3f75029a46
Wilfredo Ceron <wilfredo.ceron10@gmail.com>
2023-04-25 22:59:53 -0600

    Add files via upload

---
+++ sql_aspnetcore_sp_1tabla.sql
@@ -0,0 +1,26 @@
+﻿use master
+
+create database registro_mvc_1tabla
+
+use registro_mvc_1tabla
+
+create table usuarios(
+idUsuario int identity(1,1) primary key,
+nombre varchar(50),
+edad int,
+correo varchar(50)
+);
+
+create procedure sp_registrar
+@nombre varchar(50),
+@edad int,
+@correo varchar(50)
+as begin
+insert into usuarios values (@nombre, @edad, @correo);
+end
+
+
+create procedure sp_usuarios
+as begin
+select * from usuarios;
+end
